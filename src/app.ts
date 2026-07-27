7aaef63a6983ca4d208fdcfadecc682bbd8ba833
kattprinsen <1647312+Kattprinsen@users.noreply.github.com>
2026-07-27 07:54:19 +0200

    Initial setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+console.log("Hello test")
+
+function mainLoop(){
+    const number = 100;
+
+    for (let index = 0; index < number; index++) {
+        console.log(index);
+    }
+}
+
+mainLoop();

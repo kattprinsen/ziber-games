ca2d04e17bc72db1ee6f50f9bdc19abdbd126998
kattprinsen <1647312+Kattprinsen@users.noreply.github.com>
2026-08-05 12:50:28 +0200

    Refactor

--- src/components/combat.ts
+++ src/components/combat.ts
@@ -1,11 +1,16 @@
 import { Enemy } from "../types/enemies";
 import { Hero } from "../types/hero";
-import { attackDiceRoll } from "./diceRoll";
+import { attackDiceRoll, AttackResult } from "./diceRoll";
 
 /**
  * COMBAT ROUNDS
  * Hero attacks and (if) enemy saves, difference is wounds made
  */
+
+function fightMessage(attackType: AttackResult["type"], name: string) {
+    if(attackType === "critical") return console.log(`${name} lands a CRITICAL HIT!`);
+    if(attackType === "miss") return console.log(`${name} CRITICAL MISS :(`);
+}
 
 export function fight(hero: Hero, enemy: Enemy): string {
     let heroHp = hero.health;
@@ -13,15 +18,13 @@ export function fight(hero: Hero, enemy: Enemy): string {
 
     while(heroHp > 0 && enemyHp > 0) {
         const heroAttack = attackDiceRoll(hero.attack, enemy.defense);
-        if(heroAttack.type === "critical") console.log(`${hero.name} lands a CRITICAL HIT!`);
-        if(heroAttack.type === "miss") console.log(`${hero.name} CRITICAL MISS :(`);
+        fightMessage(heroAttack.type, hero.name);
         enemyHp -= heroAttack.damage;
         console.log(`${hero.name} deals ${heroAttack.damage} damage. ${enemy.name} HP: ${Math.max(0, enemyHp)}`);
         if(enemyHp <= 0) break;
 
         const enemyAttack = attackDiceRoll(enemy.attack, hero.defense);
-        if(enemyAttack.type === "critical") console.log(`${enemy.name} lands a CRITICAL HIT!`);
-        if(heroAttack.type === "miss") console.log(`${hero.name} CRITICAL MISS :(`);
+        fightMessage(enemyAttack.type, enemy.name);
         heroHp -= enemyAttack.damage;
         console.log(`${enemy.name} deals ${enemyAttack.damage} damage. ${hero.name} HP: ${Math.max(0, heroHp)}`);
     }
